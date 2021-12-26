eefafbc139e416330b563d620670ceba550b4458
sdltsky <sedletskiyna@mail.ru>
2021-12-26 22:26:16 +0500

    Добавил кнопки под операции очистки поля TextView полность и посимвольно

--- app/src/main/java/ru/gb/android_calc/MainActivity.java
+++ app/src/main/java/ru/gb/android_calc/MainActivity.java
@@ -3,8 +3,17 @@ package ru.gb.android_calc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultTextView;
+    Button calculateResultButton;
+    Button divisionOperationButton;
+    Button multiplyOperationButton;
+    Button subtractionOperationButton;
+    Button additionOperationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
